ce97f3bf782d85c593116389f97aa81aa7622f6c
cquentin26200 <qcheron@edenschool.fr>
2022-04-07 16:50:56 +0200

    add function for etst paswword

--- js/sum.test.js
+++ js/sum.test.js
@@ -1,5 +1,7 @@
 const testPassword = require("./testPassword");
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(testPassword(1, 2)).toBe(3);
+test('verify if the password is > as 12', () => {
+    expect(testPassword("12345678910Aa$")).toBe(true);
+    expect(testPassword("123az")).toBe(false);
+    expect(testPassword(123456789101112)).toBe(null);
   });
--- js/testPassword.js
+++ js/testPassword.js
@@ -1,3 +1,10 @@
-function sum(a, b) {
-    return a + b;
+function testPassword(str) {
+    if (str.length >= 12 && typeof str === "string") {
+        return true;
+    } else if (str.length < 12) {
+        return false;
+    } else {
+        return null;
+    }
   }
+  module.exports = testPassword;
